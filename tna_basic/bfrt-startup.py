d653eb0c8bf35db9a087a11de27afea7506b4dbc
Italo Valcy <italovalcy@gmail.com>
2026-04-01 17:02:21 -0300

    first commit

---
+++ tna_basic/bfrt-startup.py
@@ -0,0 +1,3 @@
+bfrt.tna_basic.pipe.MyIngress.forward.add_with_output(ingress_port=0, port=320)
+bfrt.tna_basic.pipe.MyIngress.forward.add_with_output(ingress_port=4, port=8)
+bfrt.complete_operations()
